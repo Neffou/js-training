b85154a2ed0b19185c9c5301e3fce2837b8db8d5
Neff <a.joffreau@gmail.com>
2019-03-18 11:28:27 +0100

    zerz

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -10,8 +10,16 @@
  *
  */
 
+const jadenCase = (str) => {
+    let arr = str.toLowerCase().split(' ')
+    let newarray1 = []
 
+    for (let i = 0 ; i < arr.length ; i++) {
+        newarray1.push(arr[i].charAt(0).toUpperCase()+arr[i].slice(1))
+    }
+    return newarray1.join(' ')
 
+}
 //* Begin of tests
 const assert = require('assert')
 
